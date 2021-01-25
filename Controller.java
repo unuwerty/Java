3e46d9040efc05b73580efb5fd118683f2d0c51a
unuwerty <denchik.yff@gmail.com>
2021-01-25 15:32:35 +0200

    method writer

--- Controller.java
+++ Controller.java
@@ -7,58 +7,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Controller
- */
+
 @WebServlet("/hello")
 public class Controller extends HttpServlet {
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	protected void Writers (HttpServletRequest request, HttpServletResponse response, String fname) 
+			throws ServletException, IOException {
+		try (PrintWriter writer = response.getWriter()){
+			fname = request.getParameter("fname");
+			writer.println("nick: " + fname);
+	}
+}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {    
-
-        try (PrintWriter writer = response.getWriter()){
-            String nick = request.getParameter("nick");
-        	writer.println("Nickname: " + nick);
-        }
+        Writers(request, response, request.getParameter("nick: "));
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
           
-        try (PrintWriter writer = res.getWriter()) {
-            String name = req.getParameter("username");
-            String age = req.getParameter("userage");
-            String gender = req.getParameter("gender");
-            String hobby = req.getParameter("hobby");
-            String[] visited_country = req.getParameterValues("visited_country");
-            
-            writer.println("<p>Name: " + name + "</p>");
-            writer.println("<p>Age: " + age + "</p>");
-            writer.println("<p>Gender: " + gender + "</p>");
-            writer.println("<p>Hobby: " + hobby + "</p>");
-            writer.println("<h3>country: </h3>");
+        try (PrintWriter writer = response.getWriter()) {
+        	Writers(request, response, request.getParameter("username: "));
+        	Writers(request, response, request.getParameter("userage: "));
+        	Writers(request, response, request.getParameter("gender: "));
+        	Writers(request, response, request.getParameter("hobby: "));
+            String[] visited_country = request.getParameterValues("visited_country");
+            writer.println("country: ");
             for(String country: visited_country)
-                writer.println("<li>" + country + "</li>");
+                writer.println(country);
         }
     }
-	protected void doPut(HttpServletRequest rq, HttpServletResponse rs) 
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-		try (PrintWriter writer = rs.getWriter()) {
-			String massage = rq.getParameter("Put");
-			writer.println("Massage: " + massage);
-		}
+		Writers(request, response, request.getParameter("answer: "));
+		
 	}
-	protected void doDelete(HttpServletRequest quest, HttpServletResponse resp) 
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-		try (PrintWriter writer = resp.getWriter()) {
-			String massage = quest.getParameter("Delete");
-			writer.println("Answer: " + massage);
+		Writers(request, response, request.getParameter("massage: "));
 		}
-	}
 }
