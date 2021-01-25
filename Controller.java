02b9c9b976e164b6603e02b29a41223550e3438f
unuwerty <denchik.yff@gmail.com>
2021-01-25 11:42:29 +0200

    new variation

--- Controller.java
+++ Controller.java
@@ -12,23 +12,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/hello")
 public class Controller extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-          
-        response.setContentType("text/html");
-        PrintWriter writer = response.getWriter();
+            throws ServletException, IOException {    
 
-        String nick = request.getParameter("nick");
-         
-        try {
-            writer.println("<h2>Nickname: " + nick + "</h2>");
-        } finally {
-            writer.close();  
+        try (PrintWriter writer = response.getWriter()){
+            String nick = request.getParameter("nick");
+        	writer.println("Nickname: " + nick);
         }
     }
 
@@ -38,16 +31,13 @@ public class Controller extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
           
-        response.setContentType("text/html");
-        PrintWriter writer = response.getWriter();
- 
-        String name = request.getParameter("username");
-        String age = request.getParameter("userage");
-        String gender = request.getParameter("gender");
-        String hobby = request.getParameter("hobby");
-        String[] visited_country = request.getParameterValues("visited_country");
-         
-        try {
+        try (PrintWriter writer = response.getWriter()) {
+            String name = request.getParameter("username");
+            String age = request.getParameter("userage");
+            String gender = request.getParameter("gender");
+            String hobby = request.getParameter("hobby");
+            String[] visited_country = request.getParameterValues("visited_country");
+            
             writer.println("<p>Name: " + name + "</p>");
             writer.println("<p>Age: " + age + "</p>");
             writer.println("<p>Gender: " + gender + "</p>");
@@ -55,8 +45,6 @@ public class Controller extends HttpServlet {
             writer.println("<h3>country: </h3>");
             for(String country: visited_country)
                 writer.println("<li>" + country + "</li>");
-        } finally {
-            writer.close();  
         }
     }
 }
