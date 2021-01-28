ffaa9e21e9bd8cf2a060f16703feae0df5cec467
unuwerty <denchik.yff@gmail.com>
2021-01-28 16:11:41 +0200

    last commit

--- Controller.java
+++ Controller.java
@@ -7,33 +7,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/hello")
 public class Controller extends HttpServlet {
-	
-	private void sendResponse (HttpServletRequest request, String arg, HttpServletResponse response) 
-			throws ServletException, IOException {
-		try (PrintWriter writer = response.getWriter()){
-			writer.println(arg);
-	}
-}
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {    
-		sendResponse(request, "nick: wer", response);
-    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-        sendResponse(request, "name: ewr \n userage: 345 \n gender: male \n hobby: sport \n Sport: Baseball", response);
-       
-    }
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-		sendResponse(request, "answer: ew34", response);
-		
-	}
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-		sendResponse(request, "question: 23wq?", response);
+	private void sendResponse(HttpServletRequest request, String arg, HttpServletResponse response)
+			throws ServletException, IOException {
+		try (PrintWriter writer = response.getWriter()) {
+			writer.println(arg);
 		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		sendResponse(request, "nick: wer", response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		sendResponse(request, "name: ewr \n userage: 345 \n gender: male \n hobby: sport \n Sport: Baseball", response);
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		sendResponse(request, "answer: ew34", response);
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		sendResponse(request, "question: 23wq?", response);
+	}
 }
